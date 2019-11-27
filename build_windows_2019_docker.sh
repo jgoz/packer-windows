669061a280fde1631ed1cbad628a882ff8c9c9ea
John Gozde <john@gozde.ca>
2019-11-26 23:49:18 -0700

    Add parallels config for windows_2019_docker

--- build_windows_2019_docker.sh
+++ build_windows_2019_docker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Insider ISO
 packer build \
-  --only=vmware-iso \
+  --only=parallels-iso \
   --var vhv_enable=true \
-  --var autounattend=./tmp/2019_core/Autounattend.xml \
-  --var iso_url=~/packer_cache/msdn/en_windows_server_2019_x64_dvd_4cb967d8.iso \
-  --var iso_checksum="4C5DD63EFEE50117986A2E38D4B3A3FBAF3C1C15E2E7EA1D23EF9D8AF148DD2D" \
+  --var iso_url=~/Downloads/en_windows_server_2019_updated_nov_2019_x64_dvd_56432a3e.iso \
+  --var iso_checksum="6afe58c8a34c7d665bc539cc70e546a964d51983cbf0c31e621b48240a256edb" \
   windows_2019_docker.json
